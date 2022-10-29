8f536e270d5ce46f969362132db225cf2051412a
Mateus Menezes <mateus.meneses@fieb.org.br>
2022-10-29 19:58:41 -0300

    Fix path in ROS building shell script

--- build_ros.sh
+++ build_ros.sh
@@ -1,6 +1,8 @@
 echo "Building ROS nodes"
 
-cd Examples/ROS/ORB_SLAM3
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/opt/ORB_SLAM3/Examples_old/ROS
+
+cd Examples_old/ROS/ORB_SLAM3
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
